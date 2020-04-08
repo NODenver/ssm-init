3324d31d062d11b595b17929a82fffcb8c3c13b4
ziqian <208295269@qq.com>
2020-04-08 11:28:08 +0800

    SSM项目环境初始化

---
+++ src/main/test/DataSourceTest.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @program: init
+ * @author: Mr.Zi
+ * @create: 2020-04-08 09:09
+ **/
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class DataSourceTest {
+}
